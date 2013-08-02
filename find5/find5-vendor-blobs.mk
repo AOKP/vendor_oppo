2fbb2df1ea38615d6c3d5f77b45e546d3aa88a68
maxwen <max.weninger@gmail.com>
2013-08-02 16:30:08 +0200

    find5: squashed blob updates from maxwen
    updated libacdbloader.so
    
    updated kickstart files
    
    updated mm-qcamera-daemon
    
    updated libaudcal.so
    
    use prebuilt libril.so to fix radio for now
    
    added prop libs to fix movie studio export
    
    Change-Id: Ic80c1072fb13190cdc14eaa98861f1e8affec6f8

--- find5/find5-vendor-blobs.mk
+++ find5/find5-vendor-blobs.mk
@@ -98,12 +98,14 @@ PRODUCT_COPY_FILES += \
 	vendor/oppo/find5/proprietary/lib/libqmi_encdec.so:system/lib/libqmi_encdec.so \
 	vendor/oppo/find5/proprietary/lib/libqmiservices.so:system/lib/libqmiservices.so \
 	vendor/oppo/find5/proprietary/lib/libQSEEComAPI.so:system/lib/libQSEEComAPI.so \
+	vendor/oppo/find5/proprietary/lib/libril.so:system/lib/libril.so \
 	vendor/oppo/find5/proprietary/lib/libril-qc-qmi-1.so:system/lib/libril-qc-qmi-1.so \
 	vendor/oppo/find5/proprietary/lib/libnetmgr.so:system/lib/libnetmgr.so \
 	vendor/oppo/find5/proprietary/lib/libril-qcril-hook-oem.so:system/lib/libril-qcril-hook-oem.so \
 	vendor/oppo/find5/proprietary/lib/libqcci_legacy.so:system/lib/libqcci_legacy.so \
-	vendor/oppo/find5/proprietary/lib/libdsi_netctrl.so:system/lib/libdsi_netctrl.so
-
+	vendor/oppo/find5/proprietary/lib/libdsi_netctrl.so:system/lib/libdsi_netctrl.so \
+	vendor/oppo/find5/proprietary/lib/libI420colorconvert.so:system/lib/libI420colorconvert.so \
+	vendor/oppo/find5/proprietary/lib/libmm-color-convertor.so:system/lib/libmm-color-convertor.so
 
 # Binaries
 PRODUCT_COPY_FILES += \
